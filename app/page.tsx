cd03de45094367f73e5654290e5585c8378ec687
anoopkini <anoopkini@users.noreply.github.com>
2024-10-16 21:42:11 +0000

    fixing commit issue

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
             <div>
                 <h2 className="inline">I am Anoop Kini, Software Developer @ </h2>
                 <a target="_blank" href="https://www.ericsson.com/en"><h2 className="inline">Ericsson.</h2></a>
-                <p><a href="mailto:anoopk.kini@gmail.com" before=' 📩' className="no-underline hover:underline after:content-[attr(before)] mt-2 " >Drop me a note</a></p>
+                <p><a href="mailto:anoopk.kini@gmail.com" data-before=' 📩' className="no-underline hover:underline after:content-[attr(data-before)] mt-2 " >Drop me a note</a></p>
             </div>
         </section>
     );
